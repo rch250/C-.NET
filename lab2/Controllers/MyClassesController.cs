1aba255473601fda8219d0672ec312d4c31fab34
rch250 <50688367+rch250@users.noreply.github.com>
2023-10-21 20:46:50 -0700

    Add files via upload

---
+++ lab2/Controllers/MyClassesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class MyClassesController : Controller
+    {
+        // GET: MyClasses
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
